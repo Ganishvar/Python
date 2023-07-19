57323938e12c9508b5699255f8a76e482d907df8
Ganishvar <71224468+Ganishvar@users.noreply.github.com>
2023-07-20 01:06:05 +0530

    Add files via upload

---
+++ Basic_Arithmetic/1_Positive_negative.py
@@ -0,0 +1,7 @@
+x=float(input('Enter any number: '))
+if(x>=1 and x!=0):
+    print(x,' is positive')
+if(x<0 and x!=0):
+    print(x,' is negative')
+if(x==0):
+    print('It is Zero')
---
+++ Basic_Arithmetic/2_Odd_Even.py
@@ -0,0 +1,5 @@
+num=float(input('Enter your number '))
+if(num%2==0):
+    print("Your number is even")
+if(num%2==1 and num<100):
+    print('Your number is odd')
